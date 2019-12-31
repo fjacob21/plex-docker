c3da93bda6a23449be120537066feb49fd91188c
Frederic Jacob <fjacob21@hotmail.com>
2019-12-31 04:50:52 -0800

    Fix run.sh script

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
-#!/bin/bas
-rm -rf /var/lib/plexmediaserver/Library/Application Support/Plex Media Server/plexmediaserver.pid
+#!/bin/bash
+rm -rf "/var/lib/plexmediaserver/Library/Application Support/Plex Media Server/plexmediaserver.pid"
 /usr/lib/plexmediaserver/"Plex Media Server"
